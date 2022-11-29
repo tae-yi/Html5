4c6c7513c733f62346f7b87bb4aa74b08391a76b
imtaeyi <mintchodan@naver.com>
2022-11-29 17:04:48 +0900

    객체

--- html5/calc/calc.js
+++ html5/calc/calc.js
@@ -1,17 +1,21 @@
-var buttons = document.querySelectorAll("button");
+var buttons = document.querySelectorAll('button');
+var input = document.querySelector('input');
+
 for (var i = 0; i < buttons.length; i++) {
   // 반복 실행
-  buttons[i].addEventListener("click", function (event) {
-    // EventListener → click 이벤트에 실행되는 코드
-    var text = event.target.innerText;
+  buttons[i].addEventListener('click', function (event) {
+    // EventListener -> click 이벤트에 실행되는 코드
+    var text = event.target.value; // x 대신에 * 사용
     console.log(text);
-    // 1. 변수 사용
-    var origin = document.querySelector("input").value;
-    document.querySelector("input").value = origin + text;
-    //  // 2. 식 하나로
-    //  document.querySelector('input').value =
-    //  document.querySelector('input').value + text;
-    //  // 3. 축약 연산자. input의 value에 text를 추가(연결)
-    //  document.querySelector('input').value += text;
+    switch(text) {
+      case '=': // eval 연산
+        input.value = eval(input.value);
+        break;
+      case 'AC': // 텍스트 초기화
+        input.value ='';
+        break;
+      default: // 텍스트 연결
+          input.value += text;
+    }
   });
 }
